c4854bd633922becf3a308b680df6b9229b83803
James Stephans <z1978448@turing.cs.niu.edu>
2024-08-03 18:29:17 -0500

    Fix GitHub merge error; fix table display bug

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     //Initialize the webpage with the appropriate statements to print the header and import relevant functions.
     session_start();
@@ -50,57 +49,4 @@
     echo '
     </body>
 </html>'
-=======
-<?php
-    //Initialize the webpage with the appropriate statements to print the header and import relevant functions.
-    session_start();
-    require './src/functions.php';
-    printHeader();
-
-    //Initialize the flag for a successful login attempt.
-    $successfulLogin = false;
-
-    //If the user is already logged in, log them out.
-    if(isset($_SESSION['username'])) {
-        unset($_SESSION['username']);
-        unset($_SESSION['adminLogin']);
-        resetUserPage();
-    }
-
-    //If the user has attempted to log in, validate the login attempt.
-    else if(isset($_POST["loginAttempt"])) {
-        //Fetch the user information associated with the given username.
-        $result = $local_pdo->prepare("SELECT username, password, role FROM login WHERE username=?;");
-        $result->execute(array($_POST['username']));
-        $row = $result->fetch(PDO::FETCH_NUM);
-
-        $successfulLogin = passwordValidation($row);
-
-        //If the password hash is out of date, update it in the database.
-        if(password_needs_rehash($loginInfo[1], 0)) {
-            $newHash = password_hash($_POST['password'], 0);
-            $queryStatement = "UPDATE login SET password='" . $newHash . "' WHERE password='" . $loginInfo[1] ."';";
-            $local_pdo->exec($queryStatement);
-        }
-    }
-
-    //If the user has not attempted to log in, or tried and failed, display the log in box.
-    if(!$successfulLogin) {
-        showLoginBox();
-
-        if(isset($_POST["loginAttempt"])) {
-            echo "\n<p class=\"login-error\">\nInvalid username or password.\n</p>\n";
-        }
-    }
-
-    //If the user successfully logged in, move them to their prior page or the home page, as appropriate.
-    else {
-        resetUserPage();
-    }
-
-    //Close the body and html tags after running the relevant functions.
-    echo '
-    </body>
-</html>'
->>>>>>> c6c6107c74981404b4f90b57dbdc7292f166bde2
 ?>
--- vieworder.php
+++ vieworder.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     //Initialize the webpage with the appropriate statements to print the header and import relevant functions.
     session_start();
@@ -58,65 +57,4 @@
             }
         }
     }
-=======
-<?php
-    //Initialize the webpage with the appropriate statements to print the header and import relevant functions.
-    session_start();
-    require './src/functions.php';
-    printHeader();
-
-    //Set this page as the return page from the login page.
-    $_SESSION['ReturnPage'] = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'] .
-    '?' . $_SERVER['QUERY_STRING'];
-
-    //If the user is not an administrator, redirect them to the home page with an error message.
-    if(!isset($_SESSION['adminLogin'])) {
-        $_SESSION['accessDenied'] = true;
-        header("Location: ./index.php");
-    }
-
-    //Display the interface with options for viewing orders.
-    else{
-        echo '<div class="weightbox">
-            <div>
-                <form method="GET" action="./vieworder.php" class="button-box">
-                    <input type="submit" class="seperate-button" value="View All Orders" name="all" />
-                    <input type="submit" class="seperate-button" value="Search Orders" name="search" />
-                </form>
-            </div>';
-        //Display the interface to view all orders or search for orders
-        if(isset($_GET['all'])){
-            echo '<p> All Orders </p>';
-            //TODO: get part name and price for orders
-            $stmt = $local_pdo->prepare('SELECT * FROM orders');
-            $stmt->execute();
-            $orders = $stmt->fetchAll();
-            showOrderTable($orders, $pdo);
-        }
-        //Display the interface to search for orders
-        if(isset($_GET['search'])){
-            echo '<p> Search Orders </p>';
-            echo '<p><form method="POST" action="./vieworder.php?search=Search+Orders" class="weight-elt">
-                <input type="radio" name="searchtype" value="orderno" checked /> Order Number
-                <input type="radio" name="searchtype" value="email" /> Customer Email
-                <input type="text" name="search"/>
-                <input type="submit" name="submitsearch" /></form></p>';
-            if(isset($_POST['submitsearch'])){
-                echo '<p> Search Results </p>';
-                $searchtype = $_POST['searchtype'];
-                $search = $_POST['search'];
-                if($searchtype == 'orderno' && !is_numeric($search)){
-                    echo '<p> Please Enter a Valid Number </p>';
-                }elseif($searchtype == 'email' && !filter_var($search, FILTER_VALIDATE_EMAIL)){
-                    echo '<p> Invalid email address </p>';
-                }else{
-                    $stmt = $local_pdo->prepare('SELECT * FROM orders WHERE ' . $searchtype . ' = :search');
-                    $stmt->execute(['search' => $search]);
-                    $orders = $stmt->fetchAll();
-                    showOrderTable($orders, $pdo);
-                }
-            }
-        }
-    }
->>>>>>> c6c6107c74981404b4f90b57dbdc7292f166bde2
 ?>
